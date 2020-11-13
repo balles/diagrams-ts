37fcd74983e3512dcc2519d0724e0d0ec3ff7f23
balles <michael.balles@finanzchef24.de>
2020-11-13 23:16:04 +0100

    fix(provider): label

--- src/providers/create-provider.ts
+++ src/providers/create-provider.ts
@@ -2,5 +2,10 @@ import { Node, getUniqueNodeId } from "../graph";
 
 export const createProvider = (imagePath: string) => (label: string): Node => ({
   id: getUniqueNodeId(),
-  attributes: { label, shape: "none", image: imagePath, height: 1.4 + 0.4 * ([...label].filter(char=>char==='\n').length+1)}
+  attributes: {
+    label,
+    shape: "none",
+    image: imagePath,
+    height: 1.4 + 0.2 * ([...label].filter((char) => char === "\n").length + 1),
+  },
 });
