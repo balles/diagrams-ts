e1755a2000a1f758a68506e2695176ccfd41133f
balles <michael.balles@finanzchef24.de>
2020-12-03 22:45:20 +0100

    fix(diagrams): node naming

--- scripts/generate-providers.ts
+++ scripts/generate-providers.ts
@@ -1,35 +1,79 @@
 import { promises } from "fs";
 import { join } from "path";
 import rimraf from "rimraf";
+import generationConfig from "./generation-config.json";
+
+type UpperWords = Record<string, string[]>;
+type TitleWords = Record<string, Record<string, string>>;
+type Aliases = Record<string, Record<string, Record<string, string>>>;
+
+const upperWords = generationConfig.upperWords as UpperWords;
+const titleWords = generationConfig.titleWords as TitleWords;
+const aliases = generationConfig.aliases as Aliases;
 
 const assetDirectory = "./assets";
 const targetDirectory = "./src/providers/generated";
 
-const defaultCommit = "dfd8e0a52c8c4d1c3ce95dc7161c23bb2eaf0acb";
-
 const getRepositoryUrl = ({
-  commit = defaultCommit,
+  commit,
   provider,
   category,
-  node,
+  fileName,
 }: {
   commit?: string;
   provider: string;
   category: string;
-  node: string;
+  fileName: string;
 }): string =>
-  `https://github.com/mingrammer/diagrams/raw/${commit}/resources/${provider}/${category}/${node}`;
+  `https://github.com/mingrammer/diagrams/raw/${commit}/resources/${provider}/${category}/${fileName}`;
 
-const getFactoryCode = (
-  provider: string,
-  category: string,
-  node: string
-): string => {
-  const factoryName = node.split(".")[0].toLowerCase().split("-").join("_");
+const getAliases = (fileName: string, provider: string, category: string) => {
+  const nodeName = mapFileNameToNodeName(fileName, provider);
+  if (aliases?.[provider]?.[category]?.[nodeName]) {
+    return `export const ${aliases[provider][category][nodeName]} = ${nodeName}; 
+    `;
+  }
+  return "";
+};
+
+const mapNamePart = (namePart: string, provider: string): string => {
+  if (upperWords[provider]?.includes(namePart)) {
+    return namePart.toUpperCase();
+  }
+  if (titleWords?.[provider]?.[namePart]) {
+    return titleWords[provider][namePart];
+  }
+  const [firstLetter, ...rest] = namePart;
+  return [firstLetter.toUpperCase(), ...rest].join("");
+};
+
+const mapFileNameToNodeName = (fileName: string, provider: string): string => {
+  const nameParts = fileName
+    .split(".")[0]
+    .split("-")
+    .filter((part) => part !== "");
+  return nameParts.reduce((acc, namePart) => {
+    return `${acc}${mapNamePart(namePart, provider)}`;
+  }, "");
+};
+
+const getFactoryCode = ({
+  provider,
+  category,
+  fileName,
+  commit,
+}: {
+  provider: string;
+  category: string;
+  fileName: string;
+  commit: string;
+}): string => {
+  const factoryName = mapFileNameToNodeName(fileName, provider);
   return `export const ${factoryName} = createProvider("${getRepositoryUrl({
+    commit,
     provider,
     category,
-    node,
+    fileName,
   })}");`;
 };
 
@@ -38,26 +82,41 @@ const generateProviderCategory = async ({
   category,
   srcProviderDirectory,
   targetProviderDirectory,
+  commit,
 }: {
   provider: string;
   category: string;
   srcProviderDirectory: string;
   targetProviderDirectory: string;
+  commit: string;
 }): Promise<void> => {
   const categoryFile = join(targetProviderDirectory, `${category}.ts`);
   const files = (
     await promises.readdir(join(srcProviderDirectory, category))
   ).filter((file) => !file.includes("rounded") && file.endsWith(".png"));
+
   await promises.writeFile(
     categoryFile,
     `import { createProvider } from "../../create-provider";
 
-    ${files.map((file) => getFactoryCode(provider, category, file)).join("\n")}
+    ${files
+      .map((file) =>
+        getFactoryCode({ provider, category, fileName: file, commit })
+      )
+      .join("\n")}
+
+    ${files
+      .map((file) => getAliases(file, provider, category))
+      .filter((alias) => alias !== "")
+      .join("")}
     `
   );
 };
 
-const generateProvider = async (provider: string): Promise<void> => {
+const generateProvider = async (
+  provider: string,
+  commit: string
+): Promise<void> => {
   const srcProviderDirectory = join(assetDirectory, provider);
   const targetProviderDirectory = join(targetDirectory, provider);
   await promises.mkdir(targetProviderDirectory);
@@ -71,6 +130,7 @@ const generateProvider = async (provider: string): Promise<void> => {
         category,
         srcProviderDirectory,
         targetProviderDirectory,
+        commit,
       })
     )
   );
@@ -87,15 +147,19 @@ const generateProvider = async (provider: string): Promise<void> => {
   );
 };
 
-const main = async (): Promise<void> => {
+const main = async (commit: string): Promise<void> => {
   rimraf.sync(targetDirectory);
   await promises.mkdir(targetDirectory);
   const directories = (await promises.readdir(assetDirectory)).filter(
     (scanResult) => !scanResult.startsWith(".")
   );
-  await Promise.all(directories.map(generateProvider));
+  await Promise.all(
+    directories.map((directory) => generateProvider(directory, commit))
+  );
 };
 
-main()
+const commit = process.argv[2];
+
+main(commit)
   .then(() => console.log("Done."))
   .catch((reason) => console.log(reason));
