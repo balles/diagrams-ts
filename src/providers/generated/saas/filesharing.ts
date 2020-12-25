22a4373270f5c759756a4645e910b0aef1ddcf2b
balles <balles@users.noreply.github.com>
2020-12-25 08:15:34 +0000

    chore(diagrams): auto update resources

---
+++ src/providers/generated/saas/filesharing.ts
@@ -0,0 +1,5 @@
+import { createProvider } from "../../create-provider";
+
+export const Nextcloud = createProvider(
+  "https://github.com/mingrammer/diagrams/raw/master/resources/saas/filesharing/nextcloud.png"
+);
