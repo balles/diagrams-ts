a126b1dbf01dbd43380e6ad44ea66af7caae698d
balles <michael.balles@finanzchef24.de>
2021-04-12 17:48:48 +0200

    chore(dot): choosable path

--- src/graph/render-dot.ts
+++ src/graph/render-dot.ts
@@ -3,27 +3,31 @@ import { spawn } from "child_process";
 // TODO: add Types for format
 // TODO: tests
 
-export const CliRenderer = ({
-  format,
-  outputFile,
-}: {
+export type CliRendererArgs = {
   format: string;
   outputFile: string;
-}) => async (input: string): Promise<string> => {
-  return renderDot({ input, format, outputFile });
+  dotPath?: string;
+};
+
+export const CliRenderer = (initialArgs: CliRendererArgs) => async (
+  input: string
+): Promise<string> => {
+  return renderDot({ input, ...initialArgs });
 };
 
 export const renderDot = async ({
   format,
   outputFile,
   input,
+  dotPath = "dot",
 }: {
   format: string;
   outputFile: string;
   input: string;
+  dotPath?: string;
 }): Promise<string> => {
   return new Promise((resolve, reject) => {
-    const dot = spawn("dot", [`-o${outputFile}`, `-T${format}`]);
+    const dot = spawn(dotPath, [`-o${outputFile}`, `-T${format}`]);
 
     dot.stdout.on("data", function (msg) {
       console.log(`dot: ${msg.toString()}`);
